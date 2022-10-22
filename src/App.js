02c7223d1e6b2c6bd57106019b0f1cc136738af6
Ruben <ruben.padilla@smartkode.mx>
2022-10-22 10:07:08 -0500

    primer commit se integran nav y footer sin detalles de diseño

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './myStyles.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar/>
       </header>
+      <body>
+        <h1>hello world</h1>
+      </body>
+      <Footer/>
     </div>
   );
 }
